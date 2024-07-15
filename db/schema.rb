ebc113ca382cf3a566ffeabfc44f870daa18231b
thatguyinabeanie <thatguyin@beanie.gg>
2024-07-15 12:43:48 -0500

    rubocop ignore

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-# rubocop:disable all
-ActiveRecord::Schema[7.1].define(version: 2024_07_15_141840) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_15_164110) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -132,4 +129,3 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_15_141840) do
   add_foreign_key "registrations", "users"
   add_foreign_key "tournament_formats", "games"
 end
-# rubocop:enable all
