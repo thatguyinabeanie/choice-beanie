db6d390305389b2c2330f849de0844d7607bd994
thatguyinabeanie <thatguyin@beanie.gg>
2024-07-16 18:17:36 -0500

    fix module

--- app/controllers/api/v1/users_registration_controller.rb
+++ app/controllers/api/v1/users_registration_controller.rb
@@ -1,15 +1,17 @@
-module Users
-  class RegistrationsController < Devise::RegistrationsController
-    # GET /resource/sign_up
-    # def new
-    #   super
-    # end
+module API
+  module V1
+    class RegistrationsController < Devise::RegistrationsController
+      # GET /resource/sign_up
+      # def new
+      #   super
+      # end
 
-    # # POST /resource
-    # def create
-    #   super
-    # end
+      # # POST /resource
+      # def create
+      #   super
+      # end
 
-    # Any other custom methods or overrides...
+      # Any other custom methods or overrides...
+    end
   end
 end
