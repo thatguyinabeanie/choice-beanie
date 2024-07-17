eb51b9567ec2e67319ed6c98bbac28245667ec62
thatguyinabeanie <thatguyin@beanie.gg>
2024-07-17 00:34:47 -0500

    added firstname and lastname

---
+++ app/models/tournament/phase.rb
@@ -0,0 +1,8 @@
+module Tournament
+  class Phase < ApplicationRecord
+    self.table_name = 'tournament_phases'
+    belongs_to :tournament, class_name: 'Tournament::Tournament'
+
+    belongs_to :phase, class_name: 'Phase::Base'
+  end
+end
