e68612b315cc2ea5a9676332ef49cf7d9100bdae
thatguyinabeanie <thatguyin@beanie.gg>
2024-07-20 16:20:34 -0500

    format specs

--- spec/models/tournament/format_spec.rb
+++ spec/models/tournament/format_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Tournament::Format do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_length_of(:name).is_at_most(255) }
+    it { is_expected.to validate_uniqueness_of(:name).scoped_to(:game_id) }
+    it { is_expected.to validate_presence_of(:game) }
+  end
+
+  describe 'associations' do
+    it 'belongs to a game' do
+      game = create(:game)
+      format = create(:format, game:)
+      expect(format.game).to eq(game)
+    end
+  end
 end
