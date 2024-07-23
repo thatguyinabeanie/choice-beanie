e62c6292f1447288424f2548bd35ca8cc9125093
thatguyinabeanie <thatguyin@beanie.gg>
2024-07-23 18:00:59 +0000

    player specs + pokemon sets specs

--- spec/models/tournament/pokemon_set_spec.rb
+++ spec/models/tournament/pokemon_set_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Tournament::PokemonSet do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to(:player).class_name('Tournament::Player').inverse_of(:pokemon_sets) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:ability) }
+    it { is_expected.to validate_presence_of(:tera_type) }
+    it { is_expected.to validate_presence_of(:nature) }
+    it { is_expected.to validate_presence_of(:held_item) }
+    it { is_expected.to validate_presence_of(:move1) }
+    it { is_expected.to validate_presence_of(:move2) }
+    it { is_expected.to validate_presence_of(:move3) }
+    it { is_expected.to validate_presence_of(:move4) }
+  end
 end
