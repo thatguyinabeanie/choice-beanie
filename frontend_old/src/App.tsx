31ae3516ff1d7102afb9fdb51028a068de818807
thatguyinabeanie <thatguyin@beanie.gg>
2024-07-26 00:05:47 -0500

    frontend_old

---
+++ frontend_old/src/App.tsx
@@ -0,0 +1,24 @@
+// import { Route, Routes } from 'react-router-dom'
+// import GamesTable from './components/GamesTable'
+// import DiscordLoginButton from './components/DiscordLoginButton'
+// import AuthCallback from './components/AuthCallback'
+// import TournamentDashboard from './pages/TournamentDashboard'
+// import ProtectedRoute from './components/ProtectedRoute'
+
+import image from '../public/beanie_brawl.jpeg'
+
+function App () {
+  return (
+    <div className="App">
+      {/* <Routes> */ }
+      {/* <Route path="/" element={ <p>Hello World</p> } /> */ }
+      {/* <Route path="/games" element={ <GamesTable /> } /> */ }
+
+      {/* </Routes> */ }
+      {/* <img src={ image.src } alt="logo" /> */ }
+      <p>Hello </p>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend_old/src/app/[[...slug]]/page.tsx
@@ -0,0 +1,11 @@
+import '../../index.css'
+
+import { ClientOnly } from './client'
+
+export function generateStaticParams () {
+  return [{ slug: [''] }]
+}
+
+export default function Page () {
+  return <ClientOnly />
+}
