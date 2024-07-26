5cf99347cff66674375af8b25d8e97954fa1fafd
thatguyinabeanie <thatguyin@beanie.gg>
2024-07-26 00:49:14 -0500

    remove front_end old

--- frontend_old/jest.config.ts
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transform: {
-    "^.+\\.jsx?$": "babel-jest"
-  },
-   // Other Jest configuration
-  reporters: [
-    ["jest-junit", { outputDirectory: "test-results/jest", outputName: "results.xml" }]
-  ],
-};
