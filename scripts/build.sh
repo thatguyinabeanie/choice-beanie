111500037920aede6876a12762166c0452e8651f
thatguyinabeanie <thatguyin@beanie.gg>
2024-07-26 22:24:07 -0500

    comment out build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -31,11 +31,11 @@ bundle exec rails rswag
 cd $current_dir
 echo "Generating Typescript API client..."
 npm run generate:api
-echo "Building frontend..."
-npm run build
+# echo "Building frontend..."
+# npm run build
 
 # Copy frontend build output to Rails public directory
-echo "Copying nextjs build output to Rails public directory..."
-cd $current_dir
-cp -r nextjs/dist/* rails_app/public/
+# echo "Copying nextjs build output to Rails public directory..."
+# cd $current_dir
+# cp -r nextjs/dist/* rails_app/public/
 
