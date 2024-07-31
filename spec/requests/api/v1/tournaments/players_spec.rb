19c4985451e7b74f759324a8635e4c2f47a190c5
thatguyinabeanie <thatguyin@beanie.gg>
2024-07-31 21:35:46 +0000

    fixes spec/requests players

--- spec/requests/api/v1/tournaments/players_spec.rb
+++ spec/requests/api/v1/tournaments/players_spec.rb
@@ -3,6 +3,11 @@ require_relative '../../../../support/openapi/schema_helper'
 require_relative '../../../../support/openapi/response_helper'
 
 RSpec.describe 'api/v1/organizations/:organization_id/tournaments/:tournament_id/players' do
+  let(:organization) { create(:organization) }
+  let(:organization_id) { organization.id }
+  let(:tournament) { create(:tournament, organization:) }
+  let(:tournament_id) { tournament.id }
+
   path('/api/v1/organizations/{organization_id}/tournaments/{tournament_id}/players') do
     parameter name: :organization_id, in: :path, type: :integer, description: 'ID of the Organization', required: true
     parameter name: :tournament_id, in: :path, type: :integer, description: 'ID of the Tournament', required: true
@@ -14,10 +19,6 @@ RSpec.describe 'api/v1/organizations/:organization_id/tournaments/:tournament_id
       operationId 'listPlayers'
 
       response(200, 'successful') do
-        let(:organization) { create(:organization) }
-        let(:organization_id) { organization.id }
-        let(:tournament) { create(:tournament, organization:) }
-        let(:tournament_id) { tournament.id }
         let(:players) { create_list(:player, 10, tournament:) }
 
         schema type: :array, items: { '$ref' => '#/components/schemas/Player' }
@@ -50,20 +51,16 @@ RSpec.describe 'api/v1/organizations/:organization_id/tournaments/:tournament_id
               user_id: { type: :integer },
               in_game_name: { type: :string }
             },
-            required: %w[user_id tournament_id in_game_name]
+            required: %w[user_id in_game_name]
           }
         }
       }
 
       response(201, 'created') do
-        let(:tournament) { create(:tournament) }
-        let(:tournament_id) { tournament.id }
         let(:player) do
           {
             player: {
               user_id: create(:user).id,
-              team_sheet_submitted: false,
-              tournament_id: tournament.id,
               in_game_name: 'fuecocos-strongest-soldier'
             }
           }
@@ -76,8 +73,6 @@ RSpec.describe 'api/v1/organizations/:organization_id/tournaments/:tournament_id
       end
 
       response(422, 'unprocessable entity') do
-        let(:tournament) { create(:tournament) }
-        let(:tournament_id) { tournament.id }
         let(:player) do
           {
             player: {
@@ -107,7 +102,7 @@ RSpec.describe 'api/v1/organizations/:organization_id/tournaments/:tournament_id
     end
   end
 
-  path('/api/v1/tournaments/{tournament_id}/players/{id}') do
+  path('/api/v1/organizations/{organization_id}/tournaments/{tournament_id}/players/{id}') do
     parameter name: :organization_id, in: :path, type: :integer, description: 'ID of the Organization', required: true
     parameter name: :tournament_id, in: :path, type: :integer, description: 'ID of the Tournament', required: true
 
@@ -120,8 +115,6 @@ RSpec.describe 'api/v1/organizations/:organization_id/tournaments/:tournament_id
       parameter name: :id, in: :path, type: :integer
 
       response(200, 'successful') do
-        let(:tournament) { create(:tournament) }
-        let(:tournament_id) { tournament.id }
         let(:player) { create(:player, tournament:) }
         let(:id) { player.user_id }
 
@@ -156,8 +149,6 @@ RSpec.describe 'api/v1/organizations/:organization_id/tournaments/:tournament_id
       }
 
       response(200, 'successful') do
-        let(:tournament) { create(:tournament) }
-        let(:tournament_id) { tournament.id }
         let(:tournament_player) { create(:player, tournament:) }
         let(:id) { tournament_player.user_id }
         let(:player) do
@@ -187,8 +178,6 @@ RSpec.describe 'api/v1/organizations/:organization_id/tournaments/:tournament_id
       parameter name: :tournament_id, in: :path, type: :integer
 
       response(200, 'successful') do
-        let(:tournament) { create(:tournament) }
-        let(:tournament_id) { tournament.id }
         let(:player) { create(:player, tournament:) }
         let(:id) { player.user_id }
 
@@ -198,8 +187,6 @@ RSpec.describe 'api/v1/organizations/:organization_id/tournaments/:tournament_id
       end
 
       response(404, 'not found') do
-        let(:tournament) { create(:tournament) }
-        let(:tournament_id) { tournament.id }
         let(:id) { 'invalid' }
 
         OpenApi::Response.set_example_response_metadata
