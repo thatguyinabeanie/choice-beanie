2d7dd565f66631da42b6cbde5d665ed6b7e4dd42
thatguyinabeanie <thatguyin@beanie.gg>
2024-07-31 23:51:40 -0500

    match controller

--- app/controllers/api/v1/tournament/matches_controller.rb
+++ app/controllers/api/v1/tournament/matches_controller.rb
@@ -3,22 +3,67 @@ require_relative '../../../../serializer/match_serializer'
 module Api
   module V1
     module Tournament
-      class MatchesController < AbstractApplicationController
-        before_action :set_match, only: %i[show update destroy] # rubocop:disable Rails/LexicallyScopedActionFilter
-        self.klass = ::Tournament::Match
-        self.serializer_klass = Serializer::Match
-        self.detail_serializer_klass = Serializer::MatchDetails
+      class MatchesController < ApplicationController
+        before_action :set_tournament
+        before_action :set_matches, only: %i[index create]
+        before_action :set_match, only: %i[show update destroy]
 
-        protected
+        def index
+          render json: @tournament.matches, each_serializer: Serializer::Match, status: :ok
+        end
+
+        def show
+          render json: serialize_details, status: :ok
+        end
+
+        def create
+          @match = @matches.new permitted_params.merge(tournament_id: @tournament.id)
+          if @match.save
+            render json: serialize_details, status: :created
+          else
+            render json: @match.errors, status: :unprocessable_entity
+          end
+        rescue ActionController::ParameterMissing => e
+          render json: { error: e.message }, status: :bad_request
+        end
+
+        def update
+          if @match.update! permitted_params
+            render json: serialize_details, status: :ok
+          else
+            render json: @match.errors, status: :unprocessable_entity
+          end
+        end
+
+        def destroy
+          @match.destroy!
+          render json: { message: 'Match deleted' }, status: :ok
+        end
+
+        private
+
+        def serialize_details
+          Serializer::MatchDetails.new(@match).serializable_hash
+        end
 
         def permitted_params
           params.require(:match).permit(:round_id, :tournament_id, :table_number, :player1_id, :player2_id, :winner_id, :loser_id, :player_one_check_in, :player_two_check_in)
         end
 
-        private
+        def set_tournament
+          @tournament ||= ::Tournament::Tournament.find(params[:tournament_id])
+          @tournament
+        rescue ActiveRecord::RecordNotFound
+          render json: { error: 'Tournament not found' }, status: :not_found
+        end
 
         def set_match
-          @match = set_object
+          @match = @tournament.matches.find(params[:id])
+        end
+
+        def set_matches
+          @matches ||= @tournament.matches
+          @matches
         end
       end
     end
