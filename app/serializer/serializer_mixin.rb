73e6fa67f6561feb6787eeb7aeac9820c4a7be8a
thatguyinabeanie <thatguyin@beanie.gg>
2024-07-31 23:56:39 -0500

    serializer mixin

---
+++ app/serializer/serializer_mixin.rb
@@ -0,0 +1,22 @@
+module SerializerMixin
+  module Name
+    extend ActiveSupport::Concern
+    included do
+      attribute :name
+    end
+  end
+
+  module Id
+    extend ActiveSupport::Concern
+    included do
+      attribute :id
+    end
+  end
+
+  module Timestamp
+    extend ActiveSupport::Concern
+    included do
+      attributes :created_at, :updated_at
+    end
+  end
+end
