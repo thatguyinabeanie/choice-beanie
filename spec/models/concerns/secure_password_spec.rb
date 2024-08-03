40fb8dbce1f08767bfece3ee3161929dac5d2654
thatguyinabeanie <thatguyin@beanie.gg>
2024-08-02 20:38:18 -0500

    get rid of password references

--- spec/models/concerns/secure_password_spec.rb
+++ spec/models/concerns/secure_password_spec.rb
@@ -23,93 +23,77 @@ RSpec.describe SecurePassword do
 
   describe '.complexity_check' do
     it 'returns true for a complex password' do
-      password = 'Abc123!@#'
-      expect(described_class.complexity_check(password)).to be true
+      expect(described_class.complexity_check('Abc123!@#')).to be true
     end
 
     it 'returns false for a password without lowercase characters' do
-      password = 'ABC123!@#'
-      expect(described_class.complexity_check(password)).to be false
+      expect(described_class.complexity_check('ABC123!@#')).to be false
     end
 
     it 'returns false for a password without uppercase characters' do
-      password = 'abc123!@#'
-      expect(described_class.complexity_check(password)).to be false
+      expect(described_class.complexity_check('abc123!@#')).to be false
     end
 
     it 'returns false for a password without digits' do
-      password = 'Abcdef!@#'
-      expect(described_class.complexity_check(password)).to be false
+      expect(described_class.complexity_check('Abcdef!@#')).to be false
     end
 
     it 'returns false for a password without special characters' do
-      password = 'Abc123456'
-      expect(described_class.complexity_check(password)).to be false
+      expect(described_class.complexity_check('Abc123456')).to be false
     end
 
     it 'returns false for a password with insufficient length' do
-      password = 'Abc123!'
-      expect(described_class.complexity_check(password)).to be false
+      expect(described_class.complexity_check('Abc123!')).to be false
     end
   end
 
   describe '.lowercase?' do
     it 'returns true for a password with lowercase characters' do
-      password = 'abc123!@#'
-      expect(described_class.lowercase?(password)).to be true
+      expect(described_class.lowercase?('abc123!@#')).to be true
     end
 
     it 'returns false for a password without lowercase characters' do
-      password = 'ABC123!@#'
-      expect(described_class.lowercase?(password)).to be false
+      expect(described_class.lowercase?('ABC123!@#')).to be false
     end
   end
 
   describe '.uppercase?' do
     it 'returns true for a password with uppercase characters' do
-      password = 'ABC123!@#'
-      expect(described_class.uppercase?(password)).to be true
+      expect(described_class.uppercase?('ABC123!@#')).to be true
     end
 
     it 'returns false for a password without uppercase characters' do
-      password = 'abc123!@#'
-      expect(described_class.uppercase?(password)).to be false
+      expect(described_class.uppercase?('abc123!@#')).to be false
     end
   end
 
   describe '.digit?' do
     it 'returns true for a password with digits' do
-      password = 'Abc123!@#'
-      expect(described_class.digit?(password)).to be true
+      expect(described_class.digit?('Abc123!@#')).to be true
     end
 
     it 'returns false for a password without digits' do
-      password = 'Abcdef!@#'
-      expect(described_class.digit?(password)).to be false
+      expect(described_class.digit?('Abcdef!@#')).to be false
     end
   end
 
   describe '.special_char?' do
     it 'returns true for a password with special characters' do
-      password = 'Abc123!@#'
-      expect(described_class.special_char?(password)).to be true
+      expect(described_class.special_char?('Abc123!@#')).to be true
     end
 
     it 'returns false for a password without special characters' do
-      password = 'Abc123456'
-      expect(described_class.special_char?(password)).to be false
+      expect(described_class.special_char?('Abc123456')).to be false
     end
   end
 
   describe '.required_length?' do
     it 'returns true for a password with required length' do
-      password = 'Abc123!@#'
-      expect(described_class.required_length?(password)).to be true
+      expect(described_class.required_length?('Abc123!@#')).to be true
     end
 
     it 'returns false for a password with insufficient length' do
-      password = 'Abc123!'
-      expect(described_class.required_length?(password)).to be false
+      expect(described_class.required_length?('Abc123!')).to be false
     end
   end
 end
