8b617f8400abde17991b46f9f63c1b65adb6d7a5
thatguyinabeanie <thatguyin@beanie.gg>
2024-08-06 19:35:44 -0500

    org staff members

---
+++ backend/spec/models/organization_staff_member_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe OrganizationStaffMember do
+  describe 'associations' do
+    it 'belongs to an organization' do
+      association = described_class.reflect_on_association(:organization)
+      expect(association.macro).to eq(:belongs_to)
+    end
+
+    it 'belongs to a user' do
+      association = described_class.reflect_on_association(:user)
+      expect(association.macro).to eq(:belongs_to)
+    end
+  end
+
+  describe 'table name' do
+    it 'has the correct table name' do
+      expect(described_class.table_name).to eq('organization_staff_members')
+    end
+  end
+end
