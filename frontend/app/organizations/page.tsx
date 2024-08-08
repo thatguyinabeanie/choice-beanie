c212ed00b9c07dc62e4c8eab6759b7d7c79a9613
thatguyinabeanie <thatguyin@beanie.gg>
2024-08-08 09:48:39 -0500

    prettier

--- frontend/app/organizations/page.tsx
+++ frontend/app/organizations/page.tsx
@@ -5,26 +5,26 @@ import { title } from "@/components/primitives";
 import { OrganizationsApi } from "@/api";
 import OrganizationCard from "@/components/organizations/OrganizationCard";
 
-export default async function OrganizationsPage () {
+export default async function OrganizationsPage() {
   const orgsApi = new OrganizationsApi();
 
   const organizations = (await orgsApi.listOrganizations()).data;
 
   return (
     <div>
-      <h1 className={ title() }>Organizations</h1>
+      <h1 className={title()}>Organizations</h1>
       <div className="container relative flex flex-row m-4 p-5 justify-evenly">
-        { organizations.map((organization) => (
+        {organizations.map((organization) => (
           <Link
-            key={ organization.id }
-            href={ `/organizations/${organization.id}` }
+            key={organization.id}
+            href={`/organizations/${organization.id}`}
           >
             <OrganizationCard
-              key={ organization.id }
-              organization={ organization }
+              key={organization.id}
+              organization={organization}
             />
           </Link>
-        )) }
+        ))}
       </div>
     </div>
   );
--- frontend/components/organizations/OrganizationCard.tsx
+++ frontend/components/organizations/OrganizationCard.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Card, CardFooter, Image } from "@nextui-org/react";
 
@@ -13,13 +13,13 @@ const OrganizationCard = ({ organization }: OrganizationCardProps) => (
     <Image
       alt="Woman listing to music"
       className="object-cover"
-      height={ 200 }
+      height={200}
       src="https://nextui.org/images/hero-card.jpeg"
-      width={ 200 }
+      width={200}
     />
 
     <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-      <p className="text-tiny text-white/80">{ organization?.name }</p>
+      <p className="text-tiny text-white/80">{organization?.name}</p>
     </CardFooter>
   </Card>
 );
