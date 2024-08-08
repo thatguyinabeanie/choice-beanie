22032168993e44450ae404c893d5e6db6cee0a5e
thatguyinabeanie <thatguyin@beanie.gg>
2024-08-08 07:36:39 -0500

    remove spec

--- frontend/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  testPathIgnorePatterns: ["/node_modules/", "/cypress/"],
-
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-  },
-};
